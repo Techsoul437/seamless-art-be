ba9b276e9cf4b2cd2e3eb51c824ba11f7211e61f
Techsoul437 <info@techsoulsolutions@gmail.com>
2025-06-16 15:27:55 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+const MONGOURL =
+  process.env.MONGO_URL ||
+  "mongodb+srv://seamless-art:3o7E5DZ3QYUfAizf@seamless-cluster.jttnobm.mongodb.net/seamless-art";
+
+app.get("/", (req, res) => {
+  res.send("TechSoul's backend is up and running!");
+});
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("✅ Database Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Database Connection Error:", err);
+    process.exit(1);
+  });
+
+export default app;
