612dbd3678a5242556242c18af5b57a3987d81aa
Techsoul437 <info@techsoulsolutions.com>
2025-08-04 09:50:55 +0530

    order controller updated

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -41,11 +41,8 @@ export const getOrder = async (req, res) => {
 export const migrateGuestOrders = async (req, res) => {
   try {
     const userId = req.user?._id;
-    const userEmail = req.user?.email;
+    const userEmail = (req.user?.email || "").toLowerCase().trim();
     const { guestId } = req.body;
-    console.log("guestId", guestId);
-
-    console.log("req.user", req.user);
 
     if (!userId) return sendError(res, "User not authenticated", 401);
     if (!guestId) return sendError(res, "guestId is required", 400);
@@ -53,54 +50,31 @@ export const migrateGuestOrders = async (req, res) => {
     const guestOrder = await Checkout.findOne({
       guestId,
       user: { $exists: false },
+      email: userEmail,
     });
-    console.log("guestOrder", guestOrder);
 
     if (!guestOrder) {
       return sendSuccess(res, "No guest orders found", []);
     }
 
-    if (guestOrder.email !== userEmail) {
-      return sendError(
-        res,
-        "Guest order email does not match logged-in user",
-        403
-      );
-    }
+    const userOrder = await Checkout.findOne({ user: userId });
 
     let migratedOrder;
-    const userOrder = await Checkout.findOne({ user: userId });
-    console.log("userOrder", userOrder);
 
     if (userOrder) {
-      const existingProductIds = new Set(
-        userOrder.products.map((p) => p.productId.toString())
+      const combinedProducts = [...userOrder.products, ...guestOrder.products];
+
+      migratedOrder = await Checkout.findOneAndUpdate(
+        { _id: userOrder._id },
+        { $set: { products: combinedProducts } },
+        { new: true }
       );
-      console.log("existingProductIds", existingProductIds);
-
-      const newProducts = guestOrder.products.filter(
-        (p) => !existingProductIds.has(p.productId.toString())
-      );
-      console.log("newProducts", newProducts);
-
-      const allProducts = [...userOrder.products, ...newProducts];
-      console.log("allProducts", allProducts);
-
-      const dedupedProducts = Array.from(
-        new Map(allProducts.map((p) => [p.productId.toString(), p])).values()
-      );
-      console.log("dedupedProducts", dedupedProducts);
-
-      userOrder.products = dedupedProducts;
-      await userOrder.save();
-
-      migratedOrder = userOrder;
 
       await guestOrder.deleteOne();
     } else {
       guestOrder.user = userId;
       guestOrder.guestId = undefined;
-      await guestOrder.save();
+      await guestOrder.save({ validateBeforeSave: false });
 
       migratedOrder = guestOrder;
     }
