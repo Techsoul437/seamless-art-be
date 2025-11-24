378ddd248c498eed2a6409a3c338c0d20bb2c0d7
Techsoul437 <info@techsoulsolutions.com>
2025-11-24 14:25:17 +0530

    fix userprofile image

--- utils/generateInitialAvatar.js
+++ utils/generateInitialAvatar.js
@@ -4,38 +4,33 @@ import { uploadAvatarToS3 } from "../services/s3AvatarService.js";
 export const generateInitialAvatar = async (name) => {
   const initial = (name?.charAt(0) || "U").toUpperCase();
 
-  // Convert hex to RGB (Sharp needs this format)
   const background = {
     create: {
       width: 300,
       height: 300,
       channels: 3,
-      background: { r: 126, g: 50, b: 48 }, // FIXED
+      background: { r: 126, g: 50, b: 48 },
     },
   };
 
-  // SVG layer
   const textSvg = `
     <svg width="300" height="300">
       <text 
         x="50%" 
         y="50%" 
-        font-size="160" 
-        font-weight="bold" 
-        fill="white" 
+        dy="20"
+        font-size="200"
+        font-weight="700"
+        fill="white"
         text-anchor="middle"
-        dominant-baseline="central"
       >
         ${initial}
       </text>
     </svg>
   `;
 
-  const textBuffer = Buffer.from(textSvg);
-
-  // Merge background + SVG
   const finalImage = await sharp(background)
-    .composite([{ input: textBuffer, gravity: "center" }])
+    .composite([{ input: Buffer.from(textSvg), gravity: "center" }])
     .jpeg()
     .toBuffer();
 
